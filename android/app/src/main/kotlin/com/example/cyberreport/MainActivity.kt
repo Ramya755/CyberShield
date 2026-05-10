d1fd2b4565d06900f9f818451de79cd09e252d02
Ramya755 <dosapadularamya@gmail.com>
2026-05-10 12:15:23 +0530

    Clean repository structure - removed messy files

--- android/app/src/main/kotlin/com/example/cyberreport/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cyberreport
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/app_strings.dart
+++
@@ -1,4 +0,0 @@
-class AppStrings {
-  static const String appTitle = 'Cyber Scam Reporter';
-  static const String reportTitle = 'Cyber Scam Report';
-}
